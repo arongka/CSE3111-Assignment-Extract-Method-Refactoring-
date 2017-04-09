e0937927ef91b76416daa176a5b6fbf0bdbce9b4
arongka <arongka.nafse.nawer@gmail.com>
2017-04-09 07:01:01 +0600

    printBanner method extracted

--- Refactorable.java
+++ Refactorable.java
@@ -4,16 +4,14 @@ public class Refactorable {
 	private MyCollection _orders = new MyCollection();
 	private String _name = "Customer Name";
 	
-	//Main code. NO refactoring done yet. Next we will apply the extract method. 
+	//extract method applied (part-1) 
 	
 	void printOwing(){
 		Enumeration e = _orders.elements();
 		double outstanding = 0.0;
 		
-		// print banner
-		System.out.println("**************************************");
-		System.out.println("************ Customer Owes ***********");
-		System.out.println("**************************************");
+		// printBanner method extracted 
+		printBanner();
 		
 		// calculate outstanding
 		while(e.hasMoreElements()){
@@ -23,5 +21,11 @@ public class Refactorable {
 		// print details 
 		System.out.println("name: " + _name);
 		System.out.println("amount: " + outstanding);
+	}
+
+	public void printBanner() {
+		System.out.println("**************************************");
+		System.out.println("************ Customer Owes ***********");
+		System.out.println("**************************************");
 	}
 }
